9d91a094919dd697ff40523e859031086f497129
Seletex <sarr1998@hotmail.com>
2026-02-27 17:46:16 -0500

    Limpieza de repositorio y actualización de físicas

--- target/debug/deps/submarino-84795b842ea268ea.d
+++
@@ -1,8 +0,0 @@
-C:\Users\Admin\source\repos\Seletex\Rust\submarino\target\debug\deps\submarino-84795b842ea268ea.d: src\main.rs src\Modulos\mod.rs src\Modulos\Fisicas\mod.rs src\Modulos\Fisicas\ejeY.rs
-
-C:\Users\Admin\source\repos\Seletex\Rust\submarino\target\debug\deps\submarino-84795b842ea268ea.exe: src\main.rs src\Modulos\mod.rs src\Modulos\Fisicas\mod.rs src\Modulos\Fisicas\ejeY.rs
-
-src\main.rs:
-src\Modulos\mod.rs:
-src\Modulos\Fisicas\mod.rs:
-src\Modulos\Fisicas\ejeY.rs:
--- target/debug/deps/submarino.d
+++
@@ -1,8 +0,0 @@
-C:\Users\Admin\source\repos\Seletex\Rust\submarino\target\debug\deps\submarino.d: src\main.rs src\modulos\mod.rs src\modulos\fisicas\mod.rs src\modulos\fisicas\eje_y.rs
-
-C:\Users\Admin\source\repos\Seletex\Rust\submarino\target\debug\deps\submarino.exe: src\main.rs src\modulos\mod.rs src\modulos\fisicas\mod.rs src\modulos\fisicas\eje_y.rs
-
-src\main.rs:
-src\modulos\mod.rs:
-src\modulos\fisicas\mod.rs:
-src\modulos\fisicas\eje_y.rs:
